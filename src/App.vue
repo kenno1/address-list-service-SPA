12b5d07826460a2010f7b420f0572bf7c592659d
kambe.kenta <kenta.k199405@gmail.com>
2020-10-07 21:52:33 +0900

    Remake repository

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="toggleSideMenu"></v-app-bar-nav-icon>
+      <v-toolbar-title>My Address List</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <SideNav/>
+
+    <v-content>
+      <v-container fluid fill-height align-start>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<script>
+import SideNav from './components/SideNav'
+import { mapActions } from 'vuex'
+export default {
+  name: 'App',
+  components: {
+    SideNav
+  },
+  data: () => ({
+    //
+  }),
+  methods: {
+    ...mapActions(['toggleSideMenu'])
+  }
+};
+</script>
