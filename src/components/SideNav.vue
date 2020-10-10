ba20b40cf0a37e6a267fd828d7022f177969f0d3
kambe.kenta <kenta.k199405@gmail.com>
2020-10-11 01:02:16 +0900

    modified SideNav.vue display login user informations

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -3,10 +3,10 @@
     <v-list>
       <v-list-item>
         <v-list-item-avatar>
-          <img src="https://avatars2.githubusercontent.com/u/1363954?s=460&v=4">
+          <img v-if="photoURL" :src="photoURL">
         </v-list-item-avatar>
         <v-list-item-content>
-          <v-list-item-title>Kazuya Kojima</v-list-item-title>
+          <v-list-item-title>{{ userName }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
